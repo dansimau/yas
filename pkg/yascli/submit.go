9aa08d6205a5e86a764c35f1f8a8a53c5027d537
Daniel Simmons <dan@dans.im>
2025-10-14 00:24:04 +0200

    Add `yas submit --outdated` command
    Only submits restacked branches that have PRs and need updating.

--- pkg/yascli/submit.go
+++ pkg/yascli/submit.go
@@ -5,13 +5,18 @@ import (
 )
 
 type submitCmd struct {
-	Stack bool `description:"Submit all branches in the current stack" long:"stack"`
+	Stack    bool `description:"Submit all branches in the current stack" long:"stack"`
+	Outdated bool `description:"Submit all branches that need submitting" long:"outdated"`
 }
 
 func (c *submitCmd) Execute(args []string) error {
 	yasInstance, err := yas.NewFromRepository(cmd.RepoDirectory)
 	if err != nil {
 		return NewError(err.Error())
+	}
+
+	if c.Outdated {
+		return yasInstance.SubmitOutdated()
 	}
 
 	if c.Stack {
