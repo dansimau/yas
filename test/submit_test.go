92719ed24e6938ed7b23f3569b27343f7698bce0
Daniel Simmons <dan@dans.im>
2025-11-12 01:17:04 +0100

    Fix tests by switching output log to NDJSON (#53)

--- test/submit_test.go
+++ test/submit_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"os"
 	"path/filepath"
@@ -121,39 +122,40 @@ func setupMockCommandsWithPR(t *testing.T, pr mockPROptions) (cmdLogFile string,
 
 // parseCmdLog parses the command log file and returns a list of commands.
 func parseCmdLog(logFile string) ([][]string, error) {
-	data, err := os.ReadFile(logFile)
+	f, err := os.Open(logFile)
 	if err != nil {
 		return nil, err
 	}
 
-	var (
-		commands   [][]string
-		currentCmd []string
-	)
-
-	scanner := bufio.NewScanner(strings.NewReader(string(data)))
-	for scanner.Scan() {
-		line := scanner.Text()
-		switch {
-		case line == "":
-			if len(currentCmd) > 0 {
-				commands = append(commands, currentCmd)
-				currentCmd = nil
-			}
-		case strings.HasPrefix(line, "  "):
-			// Argument
-			currentCmd = append(currentCmd, strings.TrimPrefix(line, "  "))
-		default:
-			// Command name
-			currentCmd = []string{line}
+	defer func() {
+		if cerr := f.Close(); cerr != nil {
+			log.Println("failed to close command log file:", cerr)
 		}
+	}()
+
+	type cmdEntry struct {
+		Script string   `json:"script"`
+		Args   []string `json:"args"`
 	}
 
-	if len(currentCmd) > 0 {
-		commands = append(commands, currentCmd)
+	var commands [][]string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		var entry cmdEntry
+		if err := json.Unmarshal(scanner.Bytes(), &entry); err != nil {
+			return nil, err
+		}
+
+		cmd := append([]string{entry.Script}, entry.Args...)
+		commands = append(commands, cmd)
 	}
 
-	return commands, scanner.Err()
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return commands, nil
 }
 
 // findCommand finds a command in the log and returns it.
